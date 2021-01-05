37b3bf80894b99c5885b7a7aff108796b2e01dce
Emil Vikström (iut482) <emil.vikstrom@tv4.se>
2021-01-05 20:22:45 +0100

    LCT-1: Rename cluster supervisor to horde cluster supervisor

--- lib/lib_cluster_test/application.ex
+++ lib/lib_cluster_test/application.ex
@@ -15,7 +15,8 @@ defmodule LibClusterTest.Application do
       # {LibClusterTest.Worker, arg}a
       {Cluster.Supervisor, [topologies(), [name: LibClusterTest.ClusterSupervisor]]},
       {Horde.Registry, [name: LibClusterTest.Registry, keys: :unique]},
-      {Horde.DynamicSupervisor, [name: LibClusterTest.ClusterSupervisor, strategy: :one_for_one]}
+      {Horde.DynamicSupervisor,
+       [name: LibClusterTest.HordeClusterSupervisor, strategy: :one_for_one]}
     ]
 
     # See https://hexdocs.pm/elixir/Supervisor.html
